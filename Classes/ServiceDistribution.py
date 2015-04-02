e5477722b2da43005950bca496763952d989beec
Kenneth <kcc66@cornell.edu>
2015-04-02 16:44:28 -0400

    Maxwell upper bd, part 1

---
+++ Classes/ServiceDistribution.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+class ServiceDistribution:
+	# For all things related to the service time distribution. 
+	#	Pmf, cdf, random variate generation. Initialized with two
+	#	vectors: vals (which contains the support) and probs (the
+	#	corresponding probabilities) 
+	#
+	# Assumed that support already sorted in increasing order. 
+
+	def __init__(self, vals, probs):
+		self.support = vals
+		self.pmf	 = probs
+
+		# Size of support, min and max values
+		self.__N	  = len(vals)
+		self.__minVal = vals[0]
+		self.__maxVal = vals[-1]
+		self.__buildCumul()
+		self.__buildCDF()
+
+	def __buildCumul(self):
+		# Builds a vector containing the cdf evaulated only at
+		#	the breakpoints
+		self.__cumul = np.zeros(self.__N)
+		self.__cumul[0] = self.pmf[0]
+
+		for i in xrange(1, self.__N):
+			self.__cumul[i] = self.__cumul[i-1] + self.pmf[i]
+			
+	def __buildCDF(self):
+		# Builds a cdf, but defined only from zero to the
+		#	maximum value in the support
+		self.__cdf     = np.zeros(self.__maxVal+1)
+		self.__cdf[-1] = 1
+		for i in xrange(self.__N - 1):
+			left  = self.support[i]
+			right = self.support[i+1]
+			self.__cdf[left:right] = self.__cumul[i]	
+
+	def sample(self, r):
+		# Given a number 0 <= r <= 1, returns Finv(r). Can
+		# 	be used to generate a sample from this distribution	
+		#   if r a sample from a Uniform(0, 1) RV.
+		# Bisection search!
+		left  = 0
+		right = self.__N  - 1 
+		
+		while left < right:
+			test = (left+right)/2
+			if r > self.__cumul[test]:
+				left = test + 1
+			else:
+				right = test
+
+		return self.support[left]
+
+	def getCDF(self):
+		  return self.__cdf
+		  
+	def getCumul(self):
+		  return self.__cumul
+
+	def evalCDF(self, x):
+		# Given x, computes F(x)
+		if x <= 0:
+			val = 0
+		elif x >= self.__maxVal:
+			val = 1
+		else:
+			val = self.__cdf[x]
+
+		return val
+  
---
+++ MaxwellIP/MMIP.py
@@ -0,0 +1,90 @@
+from gurobipy import Model, GRB, quicksum 
+import numpy as np
+
+class ModelInstance:
+	def __init__(self, svcArea, arrStream, svcDist, m, r):	
+		self.d   = arrStream.getPN()
+		self.A   = m
+		self.r   = r
+		self.t   = svcArea.getDist()
+		self.tau = svcArea.getNBdist()
+		self.formulate(svcArea, svcDist)
+		
+	def formulate(self, svcArea, svcDist):
+		########################################################
+		###################### BEGIN MODEL
+		########################################################
+		self.m = Model()
+
+		###################### DECISION VARIABLES
+		# Will an ambulance b placed at base j? 
+		x = {}	
+		for j in svcArea.bases:
+			x[j] = self.m.addVar(lb=0, ub=1, vtype = GRB.BINARY)
+
+		# Call at node i covered by ambulance at base j?
+		y = {}
+		for i in svcArea.nodes:
+			y[i] = {}
+			for j in svcArea.bases:
+				y[i][j] = self.m.addVar(lb=0, ub=1, vtype=GRB.BINARY)
+
+		# Probability that call from node i can be serviced within r periods
+		p = {}
+		for i in svcArea.nodes:
+			p[i] = self.m.addVar(lb=0, obj=self.d[1][i])
+			
+		self.m.update()
+
+		######################### CONSTRAINTS
+		# Ambulances that can be located
+		self.m.addConstr(quicksum(x[j] for j in svcArea.bases) <= self.A)
+
+		# No coverage unless ambulance there
+		for i in svcArea.nodes:
+			for j in svcArea.bases:
+				self.m.addConstr(y[i][j] <= x[j])
+
+		# Coverage in at most one location
+		for i in svcArea.nodes:
+			self.m.addConstr(quicksum(y[i][j] for j in svcArea.bases) == 1)
+
+		# Linking p-variables to y-varaibles
+		for i in svcArea.nodes:
+			self.m.addConstr(p[i] == quicksum(y[i][j]*\
+												svcDist.evalCDF(self.r\
+																- self.t[i][j]\
+																- self.tau[i])\
+												for j in svcArea.bases))
+
+		# Dictionary containing model decision variables
+		self.v = {'x' : x, 'y' : y, 'p' : p}
+		
+	def solve(self, settings={}):	
+		# Given a fully-formulated model, as well as solver settings, solves 
+		#	the IP, and returns the model object associated with the resulting
+		#	optimal solution.
+		# Settings is a dictionary whose keys are model parameters (e.g., MIPGap,
+		#	OutputFlag), and whose values, well, duh.
+	
+		# Putting settings into effect.
+		if 'OutputFlag' in settings:
+			self.m.setParam('OutputFlag', settings['OutputFlag'])
+
+		for option in settings:
+			self.m.setParam(option, settings[option])
+
+		self.m.modelSense = GRB.MAXIMIZE
+		self.m.optimize()
+		self.m.fixed()
+
+	def getModel(self):
+		return self.m
+
+	def getDecisionVars(self):
+		return self.v
+
+	def getObjective(self):
+		# If model solved, return objective associated with optimal solution
+		return self.m.objVal
+	
